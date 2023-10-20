a585528f40724ca439771b3f7ff2aededdaec3d4
Abilash777 <79863057+Abilash777@users.noreply.github.com>
2023-10-20 15:19:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("HI Gus newbie to learn docker");
+    System.out.println("HI Gus ndfewbie to learn docker");
   }
 }
